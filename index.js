5af0035ff30db9aa14c08b92198896e2ac4b3450
Alen Kapitanker <alengamer76@gmail.com>
2022-10-29 19:13:47 +0300

    xD

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.get('/uploads/:fileName', async (req,res) => {
     console.log(file.headers)
     res.status(200).set("Content-Type", file.headers["content-type"]).send(file.data)
   } catch (error) {
-    if (error.status == 404) {
+    if (error.response.status == 404) {
       console.log(`No such key ${filename}`)
       res.sendStatus(404).end()
     } else {
