892fa450f33de9ef5ccb9295dcd1843582639aa3
Alen Kapitanker <alengamer76@gmail.com>
2022-10-29 19:02:15 +0300

    fixed buggie

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.get('/uploads/:fileName', async (req,res) => {
         fileName: filename,
         responseType: "arraybuffer"
     })
-    res.status(200).send(file.data)
+    res.status(200).set("Content-Type", file.headers["Content-Type"]).send(file.data)
   } catch (error) {
     if (error.code === 'NoSuchKey') {
       console.log(`No such key ${filename}`)
